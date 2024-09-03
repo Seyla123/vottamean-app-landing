110c53da9be035e90337bc6dc6ecede6fa630949
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-03 09:31:56 +0700

    Setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import PricingPage from './pages/PricingPage';
+import FeaturePage from './pages/FeaturePage';
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path='/features' element={<FeaturePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
