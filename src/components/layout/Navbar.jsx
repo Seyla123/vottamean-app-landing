85d2022f39e73359b1746278bf353946c3455b0c
NarakCODE <channarakluy@gmail.com>
2024-09-03 11:57:17 +0700

    Feature : Redesign responsive navbar layout

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,42 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     AppBar,
     Toolbar,
-    Typography,
     Button,
     IconButton,
     Box,
     useMediaQuery,
     useTheme,
-    Menu,
-    MenuItem,
+    Drawer,
+    List,
+    ListItem,
+    ListItemText,
+    styled,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
 import { navbarData } from '../../data/navbarData';
+import { Link } from 'react-router-dom';
+
+const FullScreenDrawer = styled(Drawer)(({ theme }) => ({
+    '& .MuiDrawer-paper': {
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        color: theme.palette.primary.contrastText,
+        padding: theme.spacing(2),
+    },
+}));
 
 const Navbar = () => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileOpen, setMobileOpen] = useState(false);
 
-    const handleMenu = event => {
-        setAnchorEl(event.currentTarget);
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
     };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const mobileMenu = (
+        <FullScreenDrawer
+            anchor='top'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+        >
+            <IconButton
+                onClick={handleDrawerToggle}
+                sx={{ position: 'absolute', top: 10, right: 10 }}
+            >
+                <CloseIcon />
+            </IconButton>
+            <List sx={{ textAlign: 'center', width: '100%' }}>
+                {navbarData.menuItems.slice(0, -2).map(item => (
+                    <ListItem
+                        key={item.title}
+                        component={Link}
+                        to={item.link}
+                        onClick={handleDrawerToggle}
+                    >
+                        <ListItemText
+                            primary={item.title}
+                            sx={{
+                                color: '#000',
+                                textAlign: 'center',
+                                '&:hover': {
+                                    color: 'text.secondary',
+                                },
+                            }}
+                        />
+                    </ListItem>
+                ))}
+                <Box sx={{ display: 'flex', gap: 2, width: '100%', mt: 4 }}>
+                    <Button
+                        sx={{ width: '100%' }}
+                        variant='outlined'
+                        component={Link}
+                        to='/signin'
+                        fullWidth
+                    >
+                        Sign In
+                    </Button>
+                    <Button
+                        sx={{ width: '100%' }}
+                        color='primary'
+                        variant='contained'
+                        component={Link}
+                        to='/signup'
+                        fullWidth
+                    >
+                        Get Started
+                    </Button>
+                </Box>
+            </List>
+        </FullScreenDrawer>
+    );
 
     return (
-        <AppBar position='static'>
+        <AppBar position='static' sx={{ backgroundColor: '#fff' }}>
             <Toolbar>
                 <Box display='flex' alignItems='center' flexGrow={1}>
-                    <img
-                        src={navbarData.logo}
-                        alt='Logo'
-                        style={{ height: 40, marginRight: theme.spacing(2) }}
-                    />
+                    <Link to='/' style={{ textDecoration: 'none' }}>
+                        <img
+                            src={navbarData.logo}
+                            alt='Logo'
+                            style={{
+                                height: 40,
+                                marginRight: theme.spacing(2),
+                            }}
+                        />
+                    </Link>
                     {!isMobile &&
                         navbarData.menuItems.slice(0, -2).map(item => (
                             <Button
@@ -50,57 +125,34 @@ const Navbar = () => {
                         ))}
                 </Box>
                 {isMobile ? (
-                    <>
-                        <IconButton
-                            edge='start'
-                            color='inherit'
-                            aria-label='menu'
-                            onClick={handleMenu}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
-                        >
-                            {navbarData.menuItems.map(item => (
-                                <MenuItem
-                                    key={item.title}
-                                    onClick={handleClose}
-                                    component={Link}
-                                    to={item.link}
-                                >
-                                    {item.title}
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </>
+                    <IconButton
+                        edge='start'
+                        aria-label='menu'
+                        onClick={handleDrawerToggle}
+                    >
+                        <MenuIcon />
+                    </IconButton>
                 ) : (
-                    <>
-                        <Button color='inherit' component={Link} to='/signin'>
+                    <Box sx={{ display: 'flex', gap: 2 }}>
+                        <Button
+                            variant='outlined'
+                            component={Link}
+                            to='/signin'
+                        >
                             Sign In
                         </Button>
                         <Button
-                            color='secondary'
+                            color='primary'
                             variant='contained'
                             component={Link}
                             to='/signup'
                         >
                             Get Started
                         </Button>
-                    </>
+                    </Box>
                 )}
             </Toolbar>
+            {mobileMenu}
         </AppBar>
     );
 };
