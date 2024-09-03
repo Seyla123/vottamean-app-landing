6cfceb2777e552b4b7e3379f00f43167618b14a9
NarakCODE <channarakluy@gmail.com>
2024-09-03 16:07:54 +0700

    Feature : Tabs design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import FaqSection from '../components/section/FaqSection'
 const HomePage = () => {
@@ -8,5 +9,22 @@ const HomePage = () => {
     </div>
   )
 }
+=======
+import React from 'react';
+import HeroSection from '../components/section/HeroSection';
+import TrustedSection from '../components/section/TrustedSection';
+import OurFeatureSection from '../components/section/OurFeatureSection';
+import { Box } from '@mui/material';
+
+const HomePage = () => {
+    return (
+        <Box>
+            <HeroSection />
+            <TrustedSection />
+            <OurFeatureSection />
+        </Box>
+    );
+};
+>>>>>>> 43dce3a (Feature : Tabs design)
 
 export default HomePage;
