7fc992c422ae2903ba6b6225182afa958254fc4b
NarakCODE <channarakluy@gmail.com>
2024-09-04 17:31:26 +0700

    Feature : Completed Feature page

--- src/pages/FeaturePage.jsx
+++ src/pages/FeaturePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import OverviewSection from "../components/section/OverviewSection";
 import HighlightSection from "../components/section/HighlightSection";
 import FaqSection from "../components/section/FaqSection";
+import ContactSection from "../components/section/ContactSection";
 import { Box } from "@mui/material";
 
 const FeaturePage = () => {
@@ -10,6 +11,7 @@ const FeaturePage = () => {
             <OverviewSection />
             <HighlightSection />
             <FaqSection />
+            <ContactSection />
         </Box>
     );
 };
