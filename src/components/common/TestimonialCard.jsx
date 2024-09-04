2b1f7d2202ef2f575f263fd39d5f9bb3fd5356ee
NarakCODE <channarakluy@gmail.com>
2024-09-04 14:29:42 +0700

    Testimonial card

--- src/components/common/TestimonialCard.jsx
+++ src/components/common/TestimonialCard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import { Card, Box } from "@mui/material";
+import { Card, CardContent, Typography, Avatar } from "@mui/material";
 
 const TestimonialCard = ({ logo, title, description, profile }) => {
-    return <div></div>;
+    return (
+        <Card>
+            <CardContent>
+                <img
+                    src={logo}
+                    alt={`${title} logo`}
+                    style={{ width: 50, height: 50 }}
+                />
+                <Typography variant="h6">{title}</Typography>
+                <Typography variant="body2">{description}</Typography>
+                <Avatar src={profile.image} alt={profile.name} />
+                <Typography variant="subtitle2">{profile.name}</Typography>
+                <Typography variant="caption">{profile.role}</Typography>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default TestimonialCard;
