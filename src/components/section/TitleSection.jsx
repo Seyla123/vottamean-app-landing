b38875e4b32b61aefec5c77d214692f0304b2070
NarakCODE <channarakluy@gmail.com>
2024-09-04 09:55:21 +0700

    Feature : Center the title section

--- src/components/section/TitleSection.jsx
+++ src/components/section/TitleSection.jsx
@@ -3,43 +3,44 @@ import { Box } from "@mui/system";
 import React from "react";
 
 const TitleSection = ({ title, description }) => {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        textAlign: "center",
-        maxWidth: "75%",
-        marginBottom: "32px",
-      }}
-    >
-      <Typography
-        variant="h3"
-        sx={{
-          fontWeight: "bold",
-          fontSize: {
-            xs: "32px",
-            sm: "48px",
-          },
-        }}
-      >
-        {title}
-      </Typography>
-      <Typography
-        variant="body1"
-        sx={{
-          mt: "18px",
-          fontSize: {
-            xs: "14px",
-            sm: "18px",
-          },
-        }}
-      >
-        {description}
-      </Typography>
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                textAlign: "center",
+                maxWidth: "600px",
+                margin: "auto",
+                py: "32px",
+            }}
+        >
+            <Typography
+                variant="h3"
+                sx={{
+                    fontWeight: "bold",
+                    fontSize: {
+                        xs: "32px",
+                        sm: "48px",
+                    },
+                }}
+            >
+                {title}
+            </Typography>
+            <Typography
+                variant="body1"
+                sx={{
+                    mt: "18px",
+                    fontSize: {
+                        xs: "14px",
+                        sm: "18px",
+                    },
+                }}
+            >
+                {description}
+            </Typography>
+        </Box>
+    );
 };
 
 export default TitleSection;
