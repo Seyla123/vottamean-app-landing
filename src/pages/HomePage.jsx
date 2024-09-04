6fa884fb75b5a26c6febf951057bf8187bb0e2e8
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-09-04 11:01:51 +0700

    feature: reset HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import PricingSection from '../components/section/PricingSection'
 
 const HomePage = () => {
   return (
-    <PricingSection />
+    <div>HomePage</div>
   )
 }
 
 export default HomePage
-
-// import React from 'react'
-
-// const HomePage = () => {
-//   return (
-//     <div>HomePage</div>
-//   )
-// }
-
-// export default HomePage
