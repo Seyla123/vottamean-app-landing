41e24ab02946f6d8e6bde2a697329758099c3894
NarakCODE <channarakluy@gmail.com>
2024-09-04 11:35:27 +0700

    Feature : Highlight section & Overview section

--- src/pages/FeaturePage.jsx
+++ src/pages/FeaturePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import OverviewSection from "../components/section/OverviewSection";
-
+import HighlightSection from "../components/section/HighlightSection";
 const FeaturePage = () => {
     return (
         <div>
             <OverviewSection />
+            <HighlightSection />
         </div>
     );
 };
