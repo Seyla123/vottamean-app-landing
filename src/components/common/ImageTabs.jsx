5cbe27fe038576f56516b524d3a4b45a28c9ba8d
NarakCODE <channarakluy@gmail.com>
2024-10-23 09:56:20 +0700

    feature : add the icon

--- src/components/common/ImageTabs.jsx
+++ src/components/common/ImageTabs.jsx
@@ -10,29 +10,42 @@ import {
 } from "../ui/tab";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
+import { UserRound, ListChecks, FileSearch2, ChartPie } from "lucide-react";
 
 const tabs = [
   {
-    title: "How do UI components improve UX?",
+    title: "Log In Easily",
     id: "improve",
+    icon: <UserRound size={24} />,
     description:
-      "UI components can improve UX by providing familiar, consistent interactions that make it easy for users to navigate and interact with an application.",
+      "Teachers and administrators log in through a secure, user-friendly interface. With just a few clicks, they can access the dashboard where they can view classes, students, and daily schedules.",
     imageUrl:
       "https://images.unsplash.com/photo-1709949908058-a08659bfa922?q=80&w=1200&auto=format",
   },
   {
-    title: "Important of UI component.",
+    title: "Real-Time Attendance Marking",
     id: "important",
+    icon: <ListChecks size={24} />,
     description:
-      "Some common challenges include maintaining consistency across different devices and screen sizes, ensuring compatibility with various browsers and assistive technologies, and balancing flexibility with ease of use.",
+      "Teachers can mark attendance in real-time directly from their devices. Whether it’s for one student or the entire class, attendance is recorded instantly, reducing errors and time spent on manual logging.",
     imageUrl:
       "https://images.unsplash.com/photo-1548192746-dd526f154ed9?q=80&w=1200&auto=format",
   },
   {
-    title: "Is UI and UX Same?",
+    title: "View Attendance History",
     id: "same",
+    icon: <FileSearch2 size={24} />,
     description:
-      "     Developers can ensure the responsiveness of UI components by using techniques such as fluid layouts, flexible grids, and media queries to adapt the components to different screen sizes and orientations.",
+      "Administrators and teachers can quickly access attendance history for each student. The system maintains a comprehensive record of all sessions, allowing users to monitor trends, spot absences, and provide support when necessary.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1693581176773-a5f2362209e6?q=80&w=1200&auto=format",
+  },
+  {
+    title: "Generate and Download Reports",
+    id: "work",
+    icon: <ChartPie size={24} />,
+    description:
+      "The platform generates detailed attendance reports that can be downloaded in various formats (PDF, Excel). These reports provide insights into class attendance patterns and student participation, helping schools make data-driven decisions.",
     imageUrl:
       "https://images.unsplash.com/photo-1693581176773-a5f2362209e6?q=80&w=1200&auto=format",
   },
@@ -41,7 +54,7 @@ const tabs = [
 const ImageTabs = () => {
   return (
     <>
-      <div className=" w-full h-full">
+      <div className="w-full h-full">
         <TabsProvider
           defaultValue="improve"
           className="md:grid md:grid-cols-12 items-center gap-8 justify-center"
@@ -60,7 +73,12 @@ const ImageTabs = () => {
           <TabList className="md:col-span-6">
             {tabs.map((tab, index) => (
               <TabItem key={index} value={tab.id}>
-                <TabHeader value={tab.id}>{tab.title}</TabHeader>
+                <TabHeader value={tab.id}>
+                  <span className="flex items-center gap-4">
+                    {tab.icon}
+                    {tab.title}
+                  </span>
+                </TabHeader>
                 <TabDes value={tab.id}>
                   <p className={`bg-background text-muted-foreground  p-3`}>
                     {tab.description}
@@ -75,7 +93,7 @@ const ImageTabs = () => {
               </TabItem>
             ))}
             <Button asChild className="mt-12">
-              <Link to={"/auth/signin"}>Get Started</Link>
+              <Link to={"/auth/signin"}>Try It Now</Link>
             </Button>
           </TabList>
         </TabsProvider>
