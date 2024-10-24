666cc57a815ea025df0fa2bc6c65d920aabe6adb
lovindy <seangdy085@gmail.com>
2024-10-24 11:50:18 +0700

    resolve : fix endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,54 @@
 import path from 'path';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      historyApiFallback: true,
+      open: '/auth/signin',
+      proxy: {
+        '/api/v1': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          pathRewrite: { '^/api/v1': '' },
+        },
+      },
+    },
+    build: {
+      minify: 'terser',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+      },
+      sourcemap: mode === 'development',
+      cssCodeSplit: true,
+      target: 'es2015',
+      terserOptions: {
+        compress: {
+          drop_console: mode === 'production',
+          drop_debugger: mode === 'production',
+        },
+      },
+      chunkSizeWarningLimit: 1500,
+    },
+    esbuild: {
+      minify: mode === 'production',
+      target: 'es2015',
+    },
+  };
 });
