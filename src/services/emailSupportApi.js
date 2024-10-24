30c2e4094631d27b1b5adc47340ba369ba3ea140
lovindy <seangdy085@gmail.com>
2024-10-24 11:10:06 +0700

    modify : adjust the endpoint for base url

--- src/services/emailSupportApi.js
+++ src/services/emailSupportApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const emailSupportApi = createApi({
   reducerPath: 'emailSupportApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api/v1',
+    baseUrl: 'https://web.vottamean.com/api/v1',
   }),
   endpoints: (builder) => ({
     sendEmail: builder.mutation({
