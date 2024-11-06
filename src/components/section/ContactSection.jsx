e7a18d4d92ca0a8f6f8497a85d4cc0cc991b18fb
NarakCODE <channarakluy@gmail.com>
2024-11-06 12:01:37 +0700

    feat : remove

--- src/components/section/ContactSection.jsx
+++ src/components/section/ContactSection.jsx
@@ -9,6 +9,7 @@ import SectionHeader from '@/components/common/SectionHeader';
 import { Textarea } from '../ui/textarea';
 import FAQSection from './FAQSection';
 import { useSendEmailMutation } from '@/services/emailSupportApi';
+import { Toast } from '../ui/toast';
 
 export default function ContactSection() {
   const [formData, setFormData] = useState({
@@ -23,12 +24,12 @@ export default function ContactSection() {
   // Initialize the mutation hook
   const [sendEmail, { isLoading }] = useSendEmailMutation();
 
-  const handleInputChange = (e) => {
+  const handleInputChange = e => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    setFormData(prevData => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
     try {
       // Calling the API to send the email
@@ -37,6 +38,7 @@ export default function ContactSection() {
 
       // Show snackbar message on successful submission
       setShowSnackbar(true);
+
       // Hide snackbar after 6 seconds
       setTimeout(() => {
         setShowSnackbar(false);
@@ -47,23 +49,25 @@ export default function ContactSection() {
   };
 
   return (
-    <section className="">
+    <section className=''>
       <SectionHeader subtitle={'Contact'} title={'Get in Touch'} />
-      <div className="innerWidth mx-auto space-y-4 md:space-y-8">
+      <div className='innerWidth mx-auto space-y-4 md:space-y-8'>
         {/* Snackbar for successful submission */}
-        {showSnackbar && (
-          <div className="fixed top-20 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-green-500 text-white rounded-lg shadow-lg z-101">
+        {showSnackbar ? (
+          <div className='fixed top-20 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-green-500 text-white rounded-lg shadow-lg z-101'>
             Your message has been sent successfully! Our support team will reach
             out to you shortly.
           </div>
+        ) : (
+          ''
         )}
 
         {/* Contact Info Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="p-4 md:p-8 rounded-lg border border-dashed">
-            <div className="flex flex-col h-full">
-              <MessageSquare className="w-6 h-6 mb-4 text-primary" />
-              <h3 className="text-lg font-semibold mb-2">Message us</h3>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+          <div className='p-4 md:p-8 rounded-lg border border-dashed'>
+            <div className='flex flex-col h-full'>
+              <MessageSquare className='w-6 h-6 mb-4 text-primary' />
+              <h3 className='text-lg font-semibold mb-2'>Message us</h3>
               <Typography variant={'p'}>
                 Message us using our online chat system for quick and efficient
                 support.
@@ -74,10 +78,10 @@ export default function ContactSection() {
             </div>
           </div>
 
-          <div className="p-4 md:p-8 rounded-lg border border-dashed">
-            <div className="flex flex-col h-full">
-              <Phone className="w-6 h-6 mb-4 text-primary" />
-              <h3 className="text-lg font-semibold mb-2">Call us</h3>
+          <div className='p-4 md:p-8 rounded-lg border border-dashed'>
+            <div className='flex flex-col h-full'>
+              <Phone className='w-6 h-6 mb-4 text-primary' />
+              <h3 className='text-lg font-semibold mb-2'>Call us</h3>
               <Typography variant={'p'}>
                 Let's have a chat – there's nothing quite like talking to
                 another person.
@@ -88,10 +92,10 @@ export default function ContactSection() {
             </div>
           </div>
 
-          <div className="p-4 md:p-8 rounded-lg border border-dashed">
-            <div className="flex flex-col h-full">
-              <MapPin className="w-6 h-6 mb-4 text-primary" />
-              <h3 className="text-lg font-semibold mb-2">Address</h3>
+          <div className='p-4 md:p-8 rounded-lg border border-dashed'>
+            <div className='flex flex-col h-full'>
+              <MapPin className='w-6 h-6 mb-4 text-primary' />
+              <h3 className='text-lg font-semibold mb-2'>Address</h3>
               <Typography variant={'p'}>
                 We'd be delighted to welcome you to our Head Office.
               </Typography>
@@ -103,56 +107,56 @@ export default function ContactSection() {
         </div>
 
         {/* Contact Form Card */}
-        <div className="p-4 md:p-8 rounded-lg border border-dashed">
-          <div className="mb-8">
+        <div className='p-4 md:p-8 rounded-lg border border-dashed'>
+          <div className='mb-8'>
             <Typography variant={'h3'}>Contact Us</Typography>
             <Typography variant={'p'}>
               Message us using our online chat system for quick and efficient
               support.
             </Typography>
           </div>
-          <div className="grid grid-cols-1 gap-8">
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div className="space-y-2">
-                <Label htmlFor="name">Name</Label>
+          <div className='grid grid-cols-1 gap-8'>
+            <form onSubmit={handleSubmit} className='space-y-4'>
+              <div className='space-y-2'>
+                <Label htmlFor='name'>Name</Label>
                 <Input
-                  id="name"
-                  name="name"
+                  id='name'
+                  name='name'
                   value={formData.name}
                   onChange={handleInputChange}
-                  placeholder="Your Name"
+                  placeholder='Your Name'
                   required
                 />
               </div>
 
-              <div className="space-y-2">
-                <Label htmlFor="email">Email</Label>
+              <div className='space-y-2'>
+                <Label htmlFor='email'>Email</Label>
                 <Input
-                  id="email"
-                  name="email"
-                  type="email"
+                  id='email'
+                  name='email'
+                  type='email'
                   value={formData.email}
                   onChange={handleInputChange}
-                  placeholder="vottamean@gmail.com"
+                  placeholder='example@gmail.com'
                   required
                 />
               </div>
 
-              <div className="space-y-2">
-                <Label htmlFor="message">Message</Label>
+              <div className='space-y-2'>
+                <Label htmlFor='message'>Message</Label>
                 <Textarea
                   rows={8}
-                  id="message"
-                  name="message"
+                  id='message'
+                  name='message'
                   value={formData.message}
                   onChange={handleInputChange}
-                  placeholder="Your Message"
+                  placeholder='Your Message'
                   required
                 />
               </div>
 
               <Button
-                type="submit"
+                type='submit'
                 className={cn('md:float-right')}
                 disabled={isLoading}
               >
